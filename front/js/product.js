de71a38b75bd386618549127fc3d2f66cc8c769f
ClemRea <clementrea7@gmail.com>
2023-01-25 16:49:53 +0100

    finalisation de la page product, début du traitement du LocalStorage

--- front/js/product.js
+++ front/js/product.js
@@ -6,9 +6,10 @@ fetch("http://localhost:3000/api/products/" + id)
   .then((reponse) => reponse.json())
   .then((product) => {
     displayProduct(product);
+    listenForCartAddittion(product);
   });
 
-// Fonction pour Générer les Porduits
+// Fonction pour Générer les Porduits depuis le lien
 function getIdFromULR() {
   const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
@@ -18,18 +19,57 @@ function getIdFromULR() {
 
 // Fonction pour afficher les éléments
 function displayProduct(product) {
+  const titreProduit = document.createElement("h1");
+  titreProduit.innerText = product.name;
+  document.querySelector("#title").appendChild(titreProduit);
+
   const imageProduit = document.createElement("img");
   imageProduit.setAttribute("src", product.imageUrl);
   imageProduit.setAttribute("alt", product.altTxt);
   document.querySelector(".item__img").appendChild(imageProduit);
 
-  const prix = document.createElement("p");
+  const prix = document.createElement("span");
   prix.innerText = product.price;
   document.querySelector("#price").appendChild(prix);
 
   const descriptionProduit = document.createElement("p");
   descriptionProduit.innerText = product.description;
   document.querySelector("#description").appendChild(descriptionProduit);
+
+  // Récupération de la couleur choisie
+  const choixCouleur = document.querySelector("#colors");
+  product.colors.forEach((color) => {
+    const option = document.createElement("option");
+    option.innerText = color;
+    option.setAttribute("value", color);
+    choixCouleur.appendChild(option);
+  });
 }
 
-const choixCouleur = document.querySelector("#colors");
+// Fonction pour vérifier si les éléments sont valides et les ajoutes au panier
+function listenForCartAddittion(product) {
+  document.querySelector("#addToCart").addEventListener("click", () => {
+    const color = document.querySelector("#colors").value;
+
+    // Vérification si la couleur est valide
+    if (color === "") {
+      alert("Merci de sélectionner une couleur");
+      return;
+    }
+
+    const quantite = document.querySelector("#quantity").value;
+    // Vérification si la quantité est valide
+    if (quantite < 1 || quantite > 100) {
+      alert("Merci d'entrer une quantité comprise entre 1 et 100");
+      return;
+    }
+
+    const canap = {
+      nom: product.name,
+      id: id,
+      couleur: color,
+      quantite: quantite,
+    };
+    localStorage.setItem("panier", JSON.stringify(canap));
+  });
+}
