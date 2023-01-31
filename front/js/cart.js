139dd25e5593fb52127ae7c8de55a1875e5e75ed
ClemRea <clementrea7@gmail.com>
2023-01-31 11:51:45 +0100

    page pannier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,4 @@
-let produitDansLeLocalStorage = JSON.parse(localStorage.getItem("product"));
+let produitsDansLeLocalStorage = JSON.parse(localStorage.getItem("product"));
 
 fetch("http://localhost:3000/api/products/")
   .then((reponse) => reponse.json())
@@ -9,32 +9,34 @@ fetch("http://localhost:3000/api/products/")
 const positionElement = document.querySelector("#cart__items");
 
 // Afficher les produits du panier
-let structurePanier = [];
-
 function afficherPanier(product) {
-  if (produitDansLeLocalStorage === null) {
+  if (produitsDansLeLocalStorage === null) {
     const panierVide = document.querySelector("h1");
     return (panierVide.innerText = "Votre panier est vide !");
   } else {
-    console.log("panier pas vide");
-    for (let i = 0; i < produitDansLeLocalStorage.length; i++) {
+    produitsDansLeLocalStorage.forEach((produitsDansLeLocalStorage) => {
+      const idProduit = produitsDansLeLocalStorage.id;
+
+      let productFromApi = product.find((produit) => produit._id === idProduit);
+      const imageUrl = productFromApi.imageUrl;
+      const prixProduit = productFromApi.price;
+      const altTxt = productFromApi.altTxt;
+
       const imageProduit = document.createElement("img");
-      imageProduit.setAttribute("src", product.imageUrl[i]);
-      imageProduit.setAttribute("alt", product.altTxt[i]);
-      document.querySelector(".cart__item__img").appendChild(imageProduit);
+      imageProduit.setAttribute("src", imageUrl);
+      imageProduit.setAttribute("alt", altTxt);
+      document.getElementById("cart__items").appendChild(imageProduit);
 
       const nomElement = document.createElement("h2");
-      nomElement.innerText =
-        produitDansLeLocalStorage.nom[i].appendChild(nomElement);
+      // nomElement.innerText =
+      //   produitsDansLeLocalStorage.nom.appendChild(nomElement);
+      // document.querySelector(".cart__item__content");
 
       const prixElement = document.createElement("span");
-      prixElement.innerText = product.price[i];
+      prixElement.innerText = prixProduit;
 
       const quantite = document.createElement("p");
-      quantite.innerText = produitDansLeLocalStorage.quantite[i];
-      if (i == produitDansLeLocalStorage.length) {
-        positionElement.innerHTML = structurePanier;
-      }
-    }
+      quantite.innerText = produitsDansLeLocalStorage.quantite;
+    });
   }
 }
